30bd4bf9a59c91eed21f0e6bb8554b024e84ff67
Trevor Duhon <taduhon1485@gmail.com>
2024-01-02 01:07:00 -0600

    Added styling to project and cleaned up code

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ console.log("Let's get this party started!");
 async function searchGiphy(term) {
   const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=z1BYC8tEEKkbNuHxRkIpCLOtBwSWGoqM`;
   const res = await axios.get(url);
-  // console.log(res);
-  // console.log(res.data);
-  console.log(res.data.data);
-  // console.log(res.data.data[0].url);
-  // console.log(res.data.data[0].images.original.url);
-  console.log(Math.floor(Math.random() * 50));
-  const $gif = res.data.data[`${Math.floor(Math.random() * 50)}`].images.original.url;
+
+  console.log(res.data.data.length)
+  const index = Math.floor(Math.random() * 50);
+  const $column = $('<div>');
+  const $gif = res.data.data[`${index}`].images.original.url;
   const $img = $('<img>');
+  $column.addClass('col-md-5 col-lg-3 col-xl-2 mr-3 mb-3');
   $img.attr('src', $gif);
-  // $img.attr('src', 'https://giphy.com/embed/26FPGI3ZgduhYYVTa');
-  $gifArea.append($img);
+  $img.addClass('w-100');
+  $column.append($img)
+  $gifArea.append($column);
 }
 
 const $form = $('#giphy-search-form');
@@ -21,47 +21,14 @@ const $input = $('#giphy-search');
 const $gifArea = $('#gif-area');
 $form.on('submit', function (e) {
   e.preventDefault();
-  // console.log("SUBMIT!");
-  // console.log($input.val());
-  searchGiphy($input.val());
-  $input.val("");
+
+  if (e.originalEvent.submitter.innerText !== "Remove GIFs") {
+    searchGiphy($input.val());
+    $input.val("");
+  }
 });
 
 const $removeBtn = $('#remove-button');
 $removeBtn.on('click', () => {
-  // e.preventDefault();
   $gifArea.empty();
 });
-
-// 'http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
-
-// async function getRandomDog() {
-//   const res = await axios.get('https://dog.ceo/api/breeds/image/random');
-//   console.log(res.data);
-//   const img = document.querySelector('#dog');
-//   img.src = res.data.message;
-// }
-
-// async function getDogByBreed(breed) {
-//   try {
-//     const url = `https://dog.ceo/api/breed/${breed}/images/random`;
-//     const res = await axios.get(url);
-//     const img = document.querySelector('#dog');
-//     img.src = res.data.message;
-//   } catch (e) { //Could also put "error" or "err" in parentheses if we want
-//     console.log(e); // this will let us know what kind of error we got, so we can make different solutions (whether that's giving a random dog image or letting the user know the error is because no internet)
-//     alert('BREED NOT FOUND!');
-//     getRandomDog();
-//     // code will still run in this case, and if the specific breed isn't found, we just do a random dog
-//   }
-// }
-
-// const form = document.querySelector('#searchform');
-// const input = document.querySelector('#search')
-// form.addEventListener('submit', function (e) {
-//   e.preventDefault();
-//   // console.log("SUBMIT!");
-//   // console.log(input.value);
-//   getDogByBreed(input.value);
-//   input.value = "";
-// })
