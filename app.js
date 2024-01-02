eba63c62d4fb281c0c59e35d6431ff8f9384b129
Trevor Duhon <taduhon1485@gmail.com>
2024-01-01 22:11:08 -0600

    Finished Part 2: Appended gif from response data to the page, and made sure multiple submits result in multiple gifs appended

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ console.log("Let's get this party started!");
 async function searchGiphy(term) {
   const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=z1BYC8tEEKkbNuHxRkIpCLOtBwSWGoqM`;
   const res = await axios.get(url);
-  console.log(res.data);
+  // console.log(res);
+  // console.log(res.data);
   console.log(res.data.data);
+  // console.log(res.data.data[0].url);
+  // console.log(res.data.data[0].images.original.url);
+  console.log(Math.floor(Math.random() * 50));
+  const $gif = res.data.data[`${Math.floor(Math.random() * 50)}`].images.original.url;
+  const $img = $('<img>');
+  $img.attr('src', $gif);
+  // $img.attr('src', 'https://giphy.com/embed/26FPGI3ZgduhYYVTa');
+  $gifArea.append($img);
 }
 
 const $form = $('#giphy-search-form');
 const $input = $('#giphy-search');
+const $gifArea = $('#gif-area');
 $form.on('submit', function (e) {
   e.preventDefault();
   // console.log("SUBMIT!");
